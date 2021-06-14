57f4b165d65b3691e4d556b447a19f435e14b76f
Shailendra Parihar <shailendra.parihar@t-systems.com>
2021-06-14 16:36:13 +0530

    m

--- src/test/java/com/baselibrary/Baseclass.java
+++ src/test/java/com/baselibrary/Baseclass.java
@@ -1,4 +1,6 @@
 package com.baselibrary;
+
+import com.config.enums.OS;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,22 +26,36 @@ public class Baseclass {
 
     public static WebDriver selectBrowser(String browser) {
 
+        if (System.getProperty("os.name").toLowerCase().contains(OS.LINUX.name().toLowerCase())) {
+            if (browser.equalsIgnoreCase(com.config.enums.Browsers.CHROME.name())) {
+                ChromeOptions chromeOptions = new ChromeOptions();
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver(chromeOptions);
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            } else if (browser.equalsIgnoreCase(com.config.enums.Browsers.IE.name())) {
 
-        if (browser.equalsIgnoreCase(com.config.enums.Browsers.CHROME.name())) {
-            ChromeOptions chromeOptions = new ChromeOptions();
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver(chromeOptions);
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        } else if (browser.equalsIgnoreCase(com.config.enums.Browsers.IE.name())) {
+                InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver(ieOptions);
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            }
+        } else if (System.getProperty("os.name").toLowerCase().contains(OS.MAC.name().toLowerCase())) {
+            if (browser.equalsIgnoreCase(com.config.enums.Browsers.CHROME.name())) {
+                ChromeOptions chromeOptions = new ChromeOptions();
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver(chromeOptions);
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            } else if (browser.equalsIgnoreCase(com.config.enums.Browsers.IE.name())) {
 
-            InternetExplorerOptions ieOptions = new InternetExplorerOptions();
-            WebDriverManager.iedriver().setup();
-            driver = new InternetExplorerDriver(ieOptions);
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver(ieOptions);
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            }
+
+
+
         }
-
-
         return driver;
     }
-
 }
